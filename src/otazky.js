18a559a9ec3911f58e328772a0d4c9a361066205
MartaKirchgessner <martakirchgessner@gmail.com>
2020-11-27 00:05:35 +0100

    Otazky do kvizu

---
+++ src/otazky.js
@@ -0,0 +1,243 @@
+const poleOtazek = [
+{id: 1, 
+  otazka: 'Když potkáš na sjezdovce kamaráda, jak ho pozdravíš?',
+  a: {
+  text: 'Tschüss',
+  hodnota: false,
+  },
+  b: {
+  text: 'Hallo',
+  hodnota: true,
+  }, 
+  c: {
+  text: 'Guten Tag',
+  hodnota: false,
+  }
+},
+
+{id: 2, 
+  otazka: 'Na čem si pochutnáš, když si objednáš Kaiserschmarrn?',
+  a: {
+  text: 'Na sladkém trhanci',
+  hodnota: true,
+  },
+  b: {
+  text: 'Na hovězím vývaru',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Na císařské roštěné',
+  hodnota: false,
+  }, 
+},
+
+{id: 3, 
+  otazka: 'Kde se naučíš lyžovat? Jak se řekne lyžařská škola?',
+  a: {
+  text: 'Schneeschule',
+  hodnota: false,
+  },
+  b: {
+  text: 'Skischule',
+  hodnota: true,
+  }, 
+  c: {
+  text: 'Fahrschule',
+  hodnota: false,
+  }, 
+},
+
+
+{id: 4, 
+  otazka: 'Co bys našel, když bys v horách hledal Loipe?',
+  a: {
+  text: 'Perníkovou chaloupku',
+  hodnota: false,
+  },
+  b: {
+  text: 'Běžeckou stopu',
+  hodnota: true,
+  }, 
+  c: {
+  text: 'Vlek',
+  hodnota: false,
+  }, 
+},
+
+{id: 5, 
+  otazka: 'Jak se rozloučíš s kamarádem?',
+  a: {
+  text: 'Tschüss',
+  hodnota: true,
+  },
+  b: {
+  text: 'Auf Wiedersehen',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Hallo',
+  hodnota: false,
+  }, 
+},
+
+{id: 6, 
+  otazka:'Co si obuješ, když chceš jít bruslit?',
+  a: {
+  text: 'Lederhose',
+  hodnota: false,
+  },
+  b: {
+  text: 'Schlittschuhe',
+  hodnota: true,
+  }, 
+  c: {
+  text: 'Schneeschuhe',
+  hodnota: false,
+  }, 
+},
+
+{id: 7, 
+  otazka:'Kdo sídlí v budově s nápisem Bergwacht?',
+  a: {
+  text: 'Rozhlas',
+  hodnota: false,
+  },
+  b: {
+  text: 'Prodejna ponožek',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Horská služba',
+  hodnota: true,
+  }, 
+},
+
+{id: 8, 
+  otazka:'Co máš v plánu, když jdeš do hor wandern?',
+  a: {
+  text: 'Jdu na túru',
+  hodnota: true,
+  },
+  b: {
+  text: 'Jdu venčit psa.',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Jdu slavit narozeniny.',
+  hodnota: true,
+  }, 
+},
+
+{id: 9, 
+  otazka:'Jak pozdravíš v Bavorsku v obchodě?',
+  a: {
+  text: 'Tschüss',
+  hodnota: false,
+  },
+  b: {
+  text: 'Grüß Gott',
+  hodnota: true,
+  }, 
+  c: {
+  text: 'Ahoj',
+  hodnota: false,
+  }, 
+},
+
+{id: 10, 
+  otazka:'Co si nasadíš na hlavu, abys neměl studené uši?',
+  a: {
+  text: 'Jacke',
+  hodnota: false,
+  },
+  b: {
+  text: 'Hose',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Mütze',
+  hodnota: true,
+  }, 
+},
+
+{id: 11, 
+  otazka:'Co si obuješ, aby Ti nebyla zima? Jak se řeknou německy sněhule?',
+  a: {
+  text: 'Schneestiefel',
+  hodnota: true,
+  },
+  b: {
+  text: 'Socken',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Strumpfhose',
+  hodnota: false,
+  }, 
+},
+
+{id: 12, 
+  otazka:'Když si v horské chatě objednáš Käsespätzle, co v nich určitě najdeš?',
+  a: {
+  text: 'Povidla',
+  hodnota: false,
+  },
+  b: {
+  text: 'Sýr',
+  hodnota: true,
+  }, 
+  c: {
+  text: 'Třešně',
+  hodnota: false,
+  }, 
+},
+
+{id: 13, 
+  otazka:'Mrzneš, ale vystoupal jsi na vrchol hory. Kde se posilníš? Jak se řekne horská chata?',
+  a: {
+  text: 'Berghütte',
+  hodnota: true,
+  },
+  b: {
+  text: 'Plattenbau',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Villa',
+  hodnota: false,
+  }, 
+},
+
+{id: 14, 
+  otazka:'V závodě máš pořadové číslo šest. Kolik to je v němčině?',
+  a: {
+  text: 'Zwei',
+  hodnota: false,
+  },
+  b: {
+  text: 'Drei',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Sechs',
+  hodnota: true,
+  }, 
+},
+
+{id: 15, 
+  otazka:'Jedeš štafetu. Tvůj tým má modrou barvu. Jak se řekne v němčině?',
+  a: {
+  text: 'Blau',
+  hodnota: true,
+  },
+  b: {
+  text: 'Gelb',
+  hodnota: false,
+  }, 
+  c: {
+  text: 'Rot',
+  hodnota: false,
+  }, 
+},
+];
+

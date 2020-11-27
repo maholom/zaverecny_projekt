2dce0fc4f77b8c11f45ed7ae93fda5ba23f500d0
Soňa Matějková <mat.sona53@gmail.com>
2020-11-27 22:15:28 +0100

    Přidány otázky

--- src/otazky.js
+++ src/otazky.js
@@ -1,243 +1,598 @@
 const poleOtazek = [
-{id: 1, 
-  otazka: 'Když potkáš na sjezdovce kamaráda, jak ho pozdravíš?',
-  a: {
-  text: 'Tschüss',
-  hodnota: false,
+  {
+    id: 1,
+    otazka: 'Když potkáš na sjezdovce kamaráda, jak ho pozdravíš?',
+    a: {
+      text: 'Tschüss',
+      hodnota: false,
+    },
+    b: {
+      text: 'Hallo',
+      hodnota: true,
+    },
+    c: {
+      text: 'Guten Tag',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Hallo',
-  hodnota: true,
-  }, 
-  c: {
-  text: 'Guten Tag',
-  hodnota: false,
-  }
-},
 
-{id: 2, 
-  otazka: 'Na čem si pochutnáš, když si objednáš Kaiserschmarrn?',
-  a: {
-  text: 'Na sladkém trhanci',
-  hodnota: true,
+  {
+    id: 2,
+    otazka: 'Na čem si pochutnáš, když si objednáš Kaiserschmarrn?',
+    a: {
+      text: 'Na sladkém trhanci',
+      hodnota: true,
+    },
+    b: {
+      text: 'Na hovězím vývaru',
+      hodnota: false,
+    },
+    c: {
+      text: 'Na císařské roštěné',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Na hovězím vývaru',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Na císařské roštěné',
-  hodnota: false,
-  }, 
-},
 
-{id: 3, 
-  otazka: 'Kde se naučíš lyžovat? Jak se řekne lyžařská škola?',
-  a: {
-  text: 'Schneeschule',
-  hodnota: false,
+  {
+    id: 3,
+    otazka: 'Kde se naučíš lyžovat? Jak se řekne lyžařská škola?',
+    a: {
+      text: 'Schneeschule',
+      hodnota: false,
+    },
+    b: {
+      text: 'Skischule',
+      hodnota: true,
+    },
+    c: {
+      text: 'Fahrschule',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Skischule',
-  hodnota: true,
-  }, 
-  c: {
-  text: 'Fahrschule',
-  hodnota: false,
-  }, 
-},
 
-
-{id: 4, 
-  otazka: 'Co bys našel, když bys v horách hledal Loipe?',
-  a: {
-  text: 'Perníkovou chaloupku',
-  hodnota: false,
+  {
+    id: 4,
+    otazka: 'Co bys našel, když bys v horách hledal Loipe?',
+    a: {
+      text: 'Perníkovou chaloupku',
+      hodnota: false,
+    },
+    b: {
+      text: 'Běžeckou stopu',
+      hodnota: true,
+    },
+    c: {
+      text: 'Vlek',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Běžeckou stopu',
-  hodnota: true,
-  }, 
-  c: {
-  text: 'Vlek',
-  hodnota: false,
-  }, 
-},
 
-{id: 5, 
-  otazka: 'Jak se rozloučíš s kamarádem?',
-  a: {
-  text: 'Tschüss',
-  hodnota: true,
+  {
+    id: 5,
+    otazka: 'Jak se rozloučíš s kamarádem?',
+    a: {
+      text: 'Tschüss',
+      hodnota: true,
+    },
+    b: {
+      text: 'Auf Wiedersehen',
+      hodnota: false,
+    },
+    c: {
+      text: 'Hallo',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Auf Wiedersehen',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Hallo',
-  hodnota: false,
-  }, 
-},
 
-{id: 6, 
-  otazka:'Co si obuješ, když chceš jít bruslit?',
-  a: {
-  text: 'Lederhose',
-  hodnota: false,
+  {
+    id: 6,
+    otazka: 'Co si obuješ, když chceš jít bruslit?',
+    a: {
+      text: 'Lederhose',
+      hodnota: false,
+    },
+    b: {
+      text: 'Schlittschuhe',
+      hodnota: true,
+    },
+    c: {
+      text: 'Schneeschuhe',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Schlittschuhe',
-  hodnota: true,
-  }, 
-  c: {
-  text: 'Schneeschuhe',
-  hodnota: false,
-  }, 
-},
 
-{id: 7, 
-  otazka:'Kdo sídlí v budově s nápisem Bergwacht?',
-  a: {
-  text: 'Rozhlas',
-  hodnota: false,
+  {
+    id: 7,
+    otazka: 'Kdo sídlí v budově s nápisem Bergwacht?',
+    a: {
+      text: 'Rozhlas',
+      hodnota: false,
+    },
+    b: {
+      text: 'Prodejna ponožek',
+      hodnota: false,
+    },
+    c: {
+      text: 'Horská služba',
+      hodnota: true,
+    },
   },
-  b: {
-  text: 'Prodejna ponožek',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Horská služba',
-  hodnota: true,
-  }, 
-},
 
-{id: 8, 
-  otazka:'Co máš v plánu, když jdeš do hor wandern?',
-  a: {
-  text: 'Jdu na túru',
-  hodnota: true,
+  {
+    id: 8,
+    otazka: 'Co máš v plánu, když jdeš do hor wandern?',
+    a: {
+      text: 'Jdu na túru',
+      hodnota: true,
+    },
+    b: {
+      text: 'Jdu venčit psa.',
+      hodnota: false,
+    },
+    c: {
+      text: 'Jdu slavit narozeniny.',
+      hodnota: true,
+    },
   },
-  b: {
-  text: 'Jdu venčit psa.',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Jdu slavit narozeniny.',
-  hodnota: true,
-  }, 
-},
 
-{id: 9, 
-  otazka:'Jak pozdravíš v Bavorsku v obchodě?',
-  a: {
-  text: 'Tschüss',
-  hodnota: false,
+  {
+    id: 9,
+    otazka: 'Jak pozdravíš v Bavorsku v obchodě?',
+    a: {
+      text: 'Tschüss',
+      hodnota: false,
+    },
+    b: {
+      text: 'Grüß Gott',
+      hodnota: true,
+    },
+    c: {
+      text: 'Ahoj',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Grüß Gott',
-  hodnota: true,
-  }, 
-  c: {
-  text: 'Ahoj',
-  hodnota: false,
-  }, 
-},
 
-{id: 10, 
-  otazka:'Co si nasadíš na hlavu, abys neměl studené uši?',
-  a: {
-  text: 'Jacke',
-  hodnota: false,
+  {
+    id: 10,
+    otazka: 'Co si nasadíš na hlavu, abys neměl studené uši?',
+    a: {
+      text: 'Jacke',
+      hodnota: false,
+    },
+    b: {
+      text: 'Hose',
+      hodnota: false,
+    },
+    c: {
+      text: 'Mütze',
+      hodnota: true,
+    },
   },
-  b: {
-  text: 'Hose',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Mütze',
-  hodnota: true,
-  }, 
-},
 
-{id: 11, 
-  otazka:'Co si obuješ, aby Ti nebyla zima? Jak se řeknou německy sněhule?',
-  a: {
-  text: 'Schneestiefel',
-  hodnota: true,
+  {
+    id: 11,
+    otazka: 'Co si obuješ, aby Ti nebyla zima? Jak se řeknou německy sněhule?',
+    a: {
+      text: 'Schneestiefel',
+      hodnota: true,
+    },
+    b: {
+      text: 'Socken',
+      hodnota: false,
+    },
+    c: {
+      text: 'Strumpfhose',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Socken',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Strumpfhose',
-  hodnota: false,
-  }, 
-},
 
-{id: 12, 
-  otazka:'Když si v horské chatě objednáš Käsespätzle, co v nich určitě najdeš?',
-  a: {
-  text: 'Povidla',
-  hodnota: false,
+  {
+    id: 12,
+    otazka:
+      'Když si v horské chatě objednáš Käsespätzle, co v nich určitě najdeš?',
+    a: {
+      text: 'Povidla',
+      hodnota: false,
+    },
+    b: {
+      text: 'Sýr',
+      hodnota: true,
+    },
+    c: {
+      text: 'Třešně',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Sýr',
-  hodnota: true,
-  }, 
-  c: {
-  text: 'Třešně',
-  hodnota: false,
-  }, 
-},
 
-{id: 13, 
-  otazka:'Mrzneš, ale vystoupal jsi na vrchol hory. Kde se posilníš? Jak se řekne horská chata?',
-  a: {
-  text: 'Berghütte',
-  hodnota: true,
+  {
+    id: 13,
+    otazka:
+      'Mrzneš, ale vystoupal jsi na vrchol hory. Kde se posilníš? Jak se řekne horská chata?',
+    a: {
+      text: 'Berghütte',
+      hodnota: true,
+    },
+    b: {
+      text: 'Plattenbau',
+      hodnota: false,
+    },
+    c: {
+      text: 'Villa',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Plattenbau',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Villa',
-  hodnota: false,
-  }, 
-},
 
-{id: 14, 
-  otazka:'V závodě máš pořadové číslo šest. Kolik to je v němčině?',
-  a: {
-  text: 'Zwei',
-  hodnota: false,
+  {
+    id: 14,
+    otazka: 'V závodě máš pořadové číslo šest. Kolik to je v němčině?',
+    a: {
+      text: 'Zwei',
+      hodnota: false,
+    },
+    b: {
+      text: 'Drei',
+      hodnota: false,
+    },
+    c: {
+      text: 'Sechs',
+      hodnota: true,
+    },
   },
-  b: {
-  text: 'Drei',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Sechs',
-  hodnota: true,
-  }, 
-},
 
-{id: 15, 
-  otazka:'Jedeš štafetu. Tvůj tým má modrou barvu. Jak se řekne v němčině?',
-  a: {
-  text: 'Blau',
-  hodnota: true,
+  {
+    id: 15,
+    otazka: 'Jedeš štafetu. Tvůj tým má modrou barvu. Jak se řekne v němčině?',
+    a: {
+      text: 'Blau',
+      hodnota: true,
+    },
+    b: {
+      text: 'Gelb',
+      hodnota: false,
+    },
+    c: {
+      text: 'Rot',
+      hodnota: false,
+    },
   },
-  b: {
-  text: 'Gelb',
-  hodnota: false,
-  }, 
-  c: {
-  text: 'Rot',
-  hodnota: false,
-  }, 
-},
+
+  {
+    id: 16,
+    otazka: 'V jakém ročním období nejčastěji lyžujeme?',
+    a: {
+      text: 'Winter',
+      hodnota: true,
+    },
+    b: {
+      text: 'Frühling',
+      hodnota: false,
+    },
+    c: {
+      text: 'Sommer',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 17,
+    otazka: 'Jakým dopravním prostředkem vyjede lyžař nahoru na kopec?',
+    a: {
+      text: 'Fahrrad',
+      hodnota: false,
+    },
+    b: {
+      text: 'Auto',
+      hodnota: false,
+    },
+    c: {
+      text: 'Seilbahn',
+      hodnota: true,
+    },
+  },
+
+  {
+    id: 18,
+    otazka: 'Co si vyzuješ, než jdeš lyžovat?',
+    a: {
+      text: 'Handschuhe',
+      hodnota: false,
+    },
+    b: {
+      text: 'Hausschuhe',
+      hodnota: true,
+    },
+    c: {
+      text: 'Mütze',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 19,
+    otazka: 'Jakou barvu má sníh?',
+    a: {
+      text: 'Dunkelbraun',
+      hodnota: false,
+    },
+    b: {
+      text: 'Weiß',
+      hodnota: true,
+    },
+    c: {
+      text: 'Lila',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 20,
+    otazka: 'Jaký je led?',
+    a: {
+      text: 'Glatt',
+      hodnota: true,
+    },
+    b: {
+      text: 'Warm',
+      hodnota: false,
+    },
+    c: {
+      text: 'Rot',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 21,
+    otazka: 'Kdo ti pomůže, když tě zavalí lavina?',
+    a: {
+      text: 'Grinch',
+      hodnota: false,
+    },
+    b: {
+      text: 'Bergrettungstdienst',
+      hodnota: true,
+    },
+    c: {
+      text: 'Weinachtsmann',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 22,
+    otazka: 'Z čeho se skáče na lyžích?',
+    a: {
+      text: 'Brücke',
+      hodnota: false,
+    },
+    b: {
+      text: 'Dach',
+      hodnota: false,
+    },
+    c: {
+      text: 'Sprungschanze',
+      hodnota: true,
+    },
+  },
+
+  {
+    id: 23,
+    otazka: 'Čím si namažeš lyže?',
+    a: {
+      text: 'Butter',
+      hodnota: false,
+    },
+    b: {
+      text: 'Skiwachs',
+      hodnota: true,
+    },
+    c: {
+      text: 'Marmelade',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 24,
+    otazka: 'Co padá v zimě z nebe?',
+    a: {
+      text: 'Schnee',
+      hodnota: true,
+    },
+    b: {
+      text: 'Eiscreme',
+      hodnota: false,
+    },
+    c: {
+      text: 'Frösche',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 25,
+    otazka: 'Jaké zvíře můžeš potkat v Alpách?',
+    a: {
+      text: 'Walfisch',
+      hodnota: false,
+    },
+    b: {
+      text: 'Elefant',
+      hodnota: false,
+    },
+    c: {
+      text: 'Gämse',
+      hodnota: true,
+    },
+  },
+
+  {
+    id: 26,
+    otazka: 'Co si oblékneš v zimě na ruce?',
+    a: {
+      text: 'Socken',
+      hodnota: false,
+    },
+    b: {
+      text: 'Handschuhe',
+      hodnota: true,
+    },
+    c: {
+      text: 'Schwimmhose',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 27,
+    otazka: 'Co má na krku sněhulák z obrázku?',
+    a: {
+      text: 'Blauer Rock',
+      hodnota: false,
+    },
+    b: {
+      text: 'Roter Schal',
+      hodnota: true,
+    },
+    c: {
+      text: 'Rosa Brille',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 28,
+    otazka: 'Co si nevezmeš na hlavu, když jdeš lyžovat?',
+    a: {
+      text: 'Topf',
+      hodnota: true,
+    },
+    b: {
+      text: 'Skihelm',
+      hodnota: false,
+    },
+    c: {
+      text: 'Skimaske',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 29,
+    otazka: 'Jaké zvíře nemůžeš potkat v Alpách?',
+    a: {
+      text: 'Kuh',
+      hodnota: false,
+    },
+    b: {
+      text: 'Ziege',
+      hodnota: false,
+    },
+    c: {
+      text: 'Löwe',
+      hodnota: true,
+    },
+  },
+
+  {
+    id: 30,
+    otazka: 'Co se nedá dělat na lyžích?',
+    a: {
+      text: 'Laufen',
+      hodnota: false,
+    },
+    b: {
+      text: 'Schlafen',
+      hodnota: true,
+    },
+    c: {
+      text: 'Fahren',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 31,
+    otazka: 'Jaké zvíře žije v Alpách?',
+    a: {
+      text: 'Zebra',
+      hodnota: false,
+    },
+    b: {
+      text: 'Strauß',
+      hodnota: false,
+    },
+    c: {
+      text: 'Bär',
+      hodnota: true,
+    },
+  },
+
+  {
+    id: 32,
+    otazka: 'Která rostlina se v Alpách nesmí trhat?',
+    a: {
+      text: 'Edelweiß',
+      hodnota: true,
+    },
+    b: {
+      text: 'Löwenzahn',
+      hodnota: false,
+    },
+    c: {
+      text: 'Mohnblume',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 33,
+    otazka: 'Které ovoce nemůžeš sbírat na horské tůře?',
+    a: {
+      text: 'Blaubeeren',
+      hodnota: false,
+    },
+    b: {
+      text: 'Bananen',
+      hodnota: true,
+    },
+    c: {
+      text: 'Erdbeeren',
+      hodnota: false,
+    },
+  },
+
+  {
+    id: 34,
+    otazka: 'Jakou barvu mají stromy na obrázku?',
+    a: {
+      text: 'Grau',
+      hodnota: false,
+    },
+    b: {
+      text: 'Gelb',
+      hodnota: false,
+    },
+    c: {
+      text: 'Grün',
+      hodnota: true,
+    },
+  },
+
+  {
+    id: 35,
+    otazka: 'Jaký kroj nosí ženy v Bavorsku?',
+    a: {
+      text: 'Pyjama',
+      hodnota: false,
+    },
+    b: {
+      text: 'Dirndl',
+      hodnota: true,
+    },
+    c: {
+      text: 'Overall',
+      hodnota: false,
+    },
+  },
 ];
-

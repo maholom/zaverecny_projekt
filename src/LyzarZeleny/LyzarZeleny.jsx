2581bde0ed8c8b8520dfe7ce00e6180a712a163a
MartaKirchgessner <martakirchgessner@gmail.com>
2020-11-28 21:22:47 +0100

    Design lyzare

--- src/LyzarZeleny/LyzarZeleny.jsx
+++ src/LyzarZeleny/LyzarZeleny.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
-export const LyzarZeleny = () => {
+export const LyzarZeleny = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       id="svg8"
-      width="210mm"
-      height="297mm"
       version="1.1"
-      viewBox="0 0 210 297"
+      viewBox="0 0 30 40"
+      className={props.className}
     >
       <defs id="defs2">
         <linearGradient id="linearGradient1479">
@@ -40,7 +39,7 @@ export const LyzarZeleny = () => {
       <g id="layer1">
         <g id="g1081">
           <ellipse
-            style={{ mixBlendMode: "normal" }}
+            style={{ mixBlendMode: 'normal' }}
             id="circle1712-7-3-4-5-5-3"
             cx="14.873"
             cy="9.109"
@@ -51,7 +50,7 @@ export const LyzarZeleny = () => {
             transform="matrix(.99161 -.12927 .15335 .98817 0 0)"
           ></ellipse>
           <path
-            style={{ mixBlendMode: "normal" }}
+            style={{ mixBlendMode: 'normal' }}
             id="path1714-1-3-4-4-6-7"
             fill="#16502d"
             fillOpacity="1"
@@ -61,14 +60,14 @@ export const LyzarZeleny = () => {
             d="M12.937 8.006a1.725 1.725 0 011.906 1.512c.189 1.22-.088 3.55-.088 3.55s1.842 1.125 2.312 1.483c.985.747.815 1.554-.419 1.1-2.003-.736-2.946-1.106-3.18-1.698-.235-.592-.304-1.847-.304-1.847l-.983.695-5.172-3.573s-.358.424-.576.453c-.218.028-.51-.138-.51-.138l.372-.911-.668-.754.812.301c-.014.002.342-.844.342-.844s.275.15.39.506c.115.356.06.72.06.72l2.227 1.534s.9-1.753 3.48-2.09z"
           ></path>
           <path
-            style={{ mixBlendMode: "normal" }}
+            style={{ mixBlendMode: 'normal' }}
             id="path1716-2-4-7-77-0-5"
             fill="#030104"
             strokeWidth="0.723"
             d="M8.657 11.9s4.166 2.733 4.728 3.149c.563.416 1.922 1.536.99 3.578-.912 2-1.915 3.974-1.915 3.974l-.794-.298-1.036-.488s1.336-2.985 1.57-3.55c.231-.564.064-.983-.424-1.328-.472-.333-1.251-.75-1.954-1.208-.696-.453-1.908-1.962-1.165-3.829z"
           ></path>
           <path
-            style={{ mixBlendMode: "normal" }}
+            style={{ mixBlendMode: 'normal' }}
             id="path1718-0-3-5-8-1-1"
             fill="none"
             stroke="#030104"
@@ -83,6 +82,4 @@ export const LyzarZeleny = () => {
       <style id="style12"></style>
     </svg>
   );
-}
-
-
+};
--- src/Welcome/index.jsx
+++ src/Welcome/index.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 import { Snowman } from '../Snowman/Snowman';
+import { LyzarCerveny } from '../LyzarCerveny/LyzarCerveny';
+import { LyzarZeleny } from '../LyzarZeleny/LyzarZeleny';
 import './style.css';
 
-export const Welcome = ({startGame}) => {
+export const Welcome = ({ startGame }) => {
+  return (
+    <>
+      <div className="welcome">
+        <p className="bubble ocas">
+          Pravidla/Spielregeln
+          <br />
+          <br />
+          Herzlich willkommen - Srdečně vítej! Připravili jsme pro tebe hru, kde
+          si procvičíš němčinu, potrápíš nervy při vyhazování a naučíš se pár
+          nových slovíček.
+          <br />
+          <br />
+          Hrají dva lyžaři. Rozdělte si, kdo bude červený a zelený sjezdař.
+          Padne-li ti šestka, vyjedeš na sjezdovku. Na každém políčku na tebe
+          čeká úkol. Pokud kvíz zodpovíš správně, postoupíš o hozený počet
+          políček. Pokud se netrefíš, zůstaneš na svém políčku, namažeš si lyže
+          a na tahu bude soupeř. Vyhrává ten, kdo jako první sjede z kopce.
+          <br />
+          <br />A teď už jen: hurá do Alp!
+        </p>
+      </div>
 
-return (
-  <>
-<div className='welcome'>
-<p className="bubble ocas">Pravidla/Spielregeln<br/>
-<br/>
-Herzlich willkommen - Srdečně vítej! Připravili jsme pro tebe hru, kde si procvičíš němčinu, potrápíš nervy při vyhazování a naučíš se pár nových slovíček.<br/>
-<br/>
-Hrají dva lyžaři. Rozdělte si, kdo bude červený a zelený sjezdař. Padne-li ti šestka, vyjedeš na sjezdovku. Na každém políčku na tebe čeká úkol. Pokud kvíz zodpovíš správně, postoupíš o hozený počet políček. Pokud se netrefíš, zůstaneš na svém políčku, namažeš si lyže a na tahu bude soupeř. Vyhrává ten, kdo jako první sjede z kopce.<br/>
-<br/>
-A teď už jen: hurá do Alp!</p>
+      <div className="container">
+        <div className="left-side">
+          <Snowman className="snowman-uvodni" />
+        </div>
+        <div className="rigth-side">
+          <LyzarCerveny className="lyzar cerveny" />
+          <LyzarZeleny className="lyzar zeleny" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-</div>
-
-<div className='container'>
-  <div className='left-side'>
-    <Snowman className='snowman-uvodni' />
-  <div className='rigth-side'>
-<button className="bublina speech" onClick={startGame}>START</button>
-
-  </div>
-  </div>
-  </div>
-</>
-    );
-  };
+//<button className="bublina speech" onClick={startGame}>START</button>

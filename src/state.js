2b50880a9289b3cdbbc10a1e9d42cbc0432dec6b
Tomas Randus <tom.randus@gmail.com>
2020-12-01 19:28:59 +0100

    Zvuk

--- src/state.js
+++ src/state.js
@@ -1,12 +1,12 @@
 const fieldCount = 45;
-export const diceMax = 3;
+export const diceMax = 6;
 
 export const initialState = {
   started: true,
   player: 1,
   dice: null,
-  quiz: true, // null - nebyl vyplnen, true - uspech, false - neuspech
-  player1: 44,
+  quiz: null, // null - nebyl vyplnen, true - uspech, false - neuspech
+  player1: 0,
   player2: 0,
 };
 
@@ -55,7 +55,7 @@ export const doTurn = (state) => {
   if (state.dice === null) {
     return {
       ...state,
-      dice: 1 + Math.round(Math.random() * 2),
+      dice: 1 + Math.round(Math.random() * diceMax - 1),
     };
   }
 
